e12fd8bcebbbeb4bfc30a6c0756dbb6839a6ff3e
Faustine Li <faustineli12@gmail.com>
2017-04-16 14:29:10 -0400

    finish breaking up weights into encoder and decoder

--- source/VAE.py
+++ source/VAE.py
@@ -4,7 +4,7 @@ class VAE(object):
 
         # initialize size of VAE 
         self.encoder_layer_sizes = input_dim + [2]
-        self.decoder_layer_sizes = [2] + output_dim[:-1] 
+        self.decoder_layer_sizes = [2] + output_dim[::-1] 
         self.total_layer_sizes   = input_dim + [2] + output_dim[::-1]
        
         self.number_encoder_layers = len(self.encoder_layer_sizes) - 1
@@ -12,11 +12,17 @@ class VAE(object):
         self.number_total_layers   = len(self.total_layer_sizes) - 1
 
         # intialize weights
-        self.weights = {}
-        for i in range(self.number_total_layers):
-            self.weights[i] = np.random.uniform(-0.1, 0.1, (self.total_layer_sizes[i], 
-                                                            self.total_layer_sizes[i+1]))
-
+        self.encoder_weights = {}
+        for i in range(self.number_encoder_layers):
+            self.encoder_weights[i] = np.random.uniform(-0.1, 0.1, 
+                                                        (self.encoder_layer_sizes[i], 
+                                                         self.encoder_layer_sizes[i+1])) 
+        self.decoder_weights = {}
+        for i in range(self.number_decoder_layers):
+            self.decoder_weights[i] = np.random.uniform(-0.1, 0.1, 
+                                                        (self.decoder_layer_sizes[i],
+                                                         self.decoder_layer_sizes[i+1]))
+        
         # set params
         self.alpha = params['alpha']
         #self.activation = params['activation']
@@ -45,29 +51,34 @@ class VAE(object):
     def feedforward(self, train_data):
         '''feedforward update step'''
         # initialize storage for activations
-        self.layer_input = {}
-        self.layer_activation = {}
+        self.encoder_input = {}
+        self.encoder_activation = {}
+        self.decoder_input = {}
+        self.decoder_activation = {}
         
-        self.layer_input[0] = train_data @ self.weights[0]
-        self.layer_activation[0] = self.activation(self.layer_input[0])
+        self.encoder_input[0]      = train_data @ self.encoder_weights[0]
+        self.encoder_activation[0] = self.activation(self.encoder_input[0])
             
         # feedforward update on encoder network
         for i in range(1, self.number_encoder_layers):
-            self.layer_input[i] = self.layer_input[i-1] @ self.weights[i]
-            self.layer_activation[i] = self.activation(self.layer_input[i])
+            self.encoder_input[i] = self.encoder_input[i-1] @ self.encoder_weights[i]
+            self.encoder_activation[i] = self.activation(self.encoder_input[i])
         
         # store output as encoded latent variable parameters
-        self.mu = self.layer_activation[self.number_encoder_layers-1][0]
-        self.sigma = self.layer_activation[self.number_encoder_layers-1][1]
+        self.mu = self.encoder_activation[i][0]
+        self.sigma = self.encoder_activation[i][1]
         
         # sample latent variable using reparameterization trick
         
-        # feedforward update on the decoder network 
-        for i in range(self.number_encoder_layers, self.number_total_layers):
-            self.layer_input[i] = self.layer_input[i-1] @ self.weights[i]
-            self.layer_activation[i] = self.activation(self.layer_input[i])
+        # feedforward update on the decoder network
+        self.decoder_input[0]      = self.encoder_activation[i] @ self.decoder_weights[0]
+        self.decoder_activation[0] = self.activation(self.decoder_input[0])
+        
+        for i in range(1, self.number_decoder_layers):
+            self.decoder_input[i] = self.decoder_input[i-1] @ self.decoder_weights[i]
+            self.decoder_activation[i] = self.activation(self.decoder_input[i])
             
         # output activation
-        self.output = self.layer_activation[self.number_total_layers - 1]
+        self.output = self.decoder_activation[i]
 
         return self.output

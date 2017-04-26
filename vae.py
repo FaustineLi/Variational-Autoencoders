7d0ac2c3561a5e94167dbd47984520c34eefef0b
Faustine Li <faustineli12@gmail.com>
2017-04-26 00:17:36 -0400

    update vae code

--- vae.py
+++ vae.py
@@ -2,13 +2,14 @@ import numpy as np
 
 class vae(object):
 
-    def __init__(self, input_dim, output_dim, params, hidden_dim = 2):
+    def __init__(self, input_dim, output_dim, params, latent_dim = 2):
         '''intializes weights matrix and parameters'''
 
-        # initialize size of VAE 
-        self.encoder_layer_sizes = input_dim + [hidden_dim]
-        self.decoder_layer_sizes = [hidden_dim] + output_dim[::-1] 
-        self.total_layer_sizes   = input_dim + [hidden_dim] + output_dim[::-1]
+        # initialize size of VAE
+        self.latent_dim = latent_dim
+        self.encoder_layer_sizes = input_dim + [2]
+        self.decoder_layer_sizes = [latent_dim] + output_dim[::-1] 
+        self.total_layer_sizes   = input_dim + [2, latent_dim] + output_dim[::-1]
        
         self.number_encoder_layers = len(self.encoder_layer_sizes) - 1
         self.number_decoder_layers = len(self.decoder_layer_sizes) - 1
@@ -30,13 +31,21 @@ class vae(object):
         self.max_iter = params['max_iter']
         self.activation = params['activation']
         self.grad_activation = params['grad_act']
-        self.loss = params['loss']
-        self.grad_loss = params['grad_loss']
+        self.reconst_loss = params['loss']
+        self.reconst_grad = params['grad_loss']
+        self.mode = params['mode']
+        
+        if self.mode is 'autoencoder':
+            self.loss = self.reconst_loss
+            self.grad_loss = self.reconst_grad
+        else:
+            self.loss = (lambda y, yhat: self.reconst_loss(y, yhat) + self.KLD_loss())
+            self.grad_loss = (lambda y, yhat: self.reconst_grad(y, yhat) + self.KLD_loss())
     
-    def KLD(self):
+    def KLD_loss(self):
         '''Kullback–Leibler divergence loss'''
-        pass
-
+        return - 0.5 * np.mean(1 + self.sigma - self.mu**2 - np.exp(self.sigma), axis=-1)
+        
     def backprop(self, X, y, yhat):
         '''back-propagation algorithm'''
         # initialize 
@@ -104,8 +113,12 @@ class vae(object):
         self.sigma = self.encoder_activation[self.number_encoder_layers - 1][:,0]
         
         # sample latent variable using reparameterization trick
-        self.z = self.encoder_activation[self.number_encoder_layers - 1] 
-        
+        if self.mode is 'vae':
+            self.epsilon = np.random.normal(0, 1, (self.mu.size, self.latent_dim))
+            self.z = self.mu + self.sigma * self.epsilon
+        else:
+            self.z = self.encoder_activation[self.number_encoder_layers - 1]
+    
         # feedforward update on the decoder network
         self.decoder_input[0]      = self.z @ self.decoder_weights[0]
         self.decoder_activation[0] = self.activation(self.decoder_input[0])
@@ -120,38 +133,31 @@ class vae(object):
     def train(self, X, y):
         '''trains the VAE model'''
         for i in range(self.max_iter):
-            
             yhat = self.feedforward(X)
             grad_encoder, grad_decoder = self.backprop(X, y, yhat)
             for i in range(self.number_decoder_layers):
                 self.decoder_weights[i] -= self.alpha * grad_decoder[i]
-            
-            change = 0
+        
             for j in range(self.number_encoder_layers):
                 self.encoder_weights[j] -= self.alpha * grad_encoder[j]
                 change = grad_encoder[j]
-        
+                
         return None
 
     def predict(self, X):
         '''predicts on a trained VAE model'''        
-        yhat = self.feedforward(X)
-        return yhat
+        return self.feedforward(X)
     
-    def generate(self):
+    def generate(self, z):
         '''generates new images from a trained VAE model'''        
-        
-        # sample from latent variable space
-        self.z = self.z
-        
         # feedforward on decoder
         self.gen_input = {}
         self.gen_activation = {}
-        self.gen_input[0]     = self.z.T @ self.decoder_weights[0]
+        self.gen_input[0]     = z.T @ self.decoder_weights[0]
         self.gen_activation[0] = self.activation(self.gen_input[0])
         
         for i in range(1, self.number_decoder_layers):
             self.gen_input[i] = self.gen_input[i-1] @ self.decoder_weights[i]
             self.gen_activation[i] = self.activation(self.gen_input[i])
 
-        return self.gen_activation[i]
+        return self.gen_activation[self.number_decoder_layers - 1]
